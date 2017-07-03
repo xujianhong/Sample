a52f3dce51b4029114561fa90c1b65184818829f
jianhongxu <571152503@qq.com>
2017-07-03 17:23:05 +0800

    remove useless stuff

--- ijkplayer_coustom/src/main/java/com/daomingedu/ijkplayertest/MainActivity.java
+++ ijkplayer_coustom/src/main/java/com/daomingedu/ijkplayertest/MainActivity.java
@@ -1,110 +1,19 @@
 package com.daomingedu.ijkplayertest;
 
-import android.content.res.Configuration;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Created by jianhongxu on 2017/7/3.
+ */
 
-import com.daomingedu.ijkplayertest.widget.PlayView;
-
-import com.daomingedu.ijkplayertest.widget.media.IjkVideoView;
-
-
-import tv.danmaku.ijk.media.player.IjkMediaPlayer;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-
-    IjkVideoView ijk;
-
-
-
-    String path ="rtmp://live.hkstv.hk.lxdns.com/live/hks";
-
-
-    Button btn_path1;
-
-    Button btn_path2;
-
-
-    private boolean mBackPressed;
-
-    PlayView playView;
-
+public  class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initView();
-    }
-
-    private void initView() {
-
-        findViewById(R.id.btn_path1).setOnClickListener(this);
-
-        findViewById(R.id.btn_path2).setOnClickListener(this);
-
-//        path = "http://baobab.wdjcdn.com/14564977406580.mp4";
-        path ="/storage/emulated/0/Movies/video_20170516_115603.mp4";
-        playView = new PlayView(this).setPath(path,false);
-
-
-    }
-
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        playView.onConfigurationChanged(newConfig);
-
-        super.onConfigurationChanged(newConfig);
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        playView.onDestroy();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        playView.onStart();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        playView.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-//        if(mBackPressed||!ijk.isBackgroundPlayEnabled()){
-//            ijk.stopPlayback();
-//            ijk.release(true);
-//            ijk.stopBackgroundPlay();//关闭后台播放
-//        }else{
-//            ijk.enterBackground();//后台播放
-//        }
-        IjkMediaPlayer.native_profileEnd();
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        if(v.getId() == R.id.btn_path1){
-            path = "/storage/emulated/0/Movies/video_20170516_115603.mp4";
-
-        }else if(v.getId() == R.id.btn_path2){
-            path ="/storage/emulated/0/Movies/video_20170418_102425.mp4";
-        }
-        playView.setPath(path,false);
     }
 }
+
